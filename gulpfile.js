ca8924f988cb3423280f6f814cafe976292a4f78
gorkolas <gorka.molero@gmail.com>
2018-06-20 13:23:29 +0200

    Moved to footer config

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const minify = require('gulp-babel-minify');
 const concat = require('gulp-concat');
 
 gulp.task("minify", () =>
-  gulp.src("./scripts/*.js")
-    .pipe(minify({
-      mangle: {
-        keepClassName: true
-      }
-    }))
-    .pipe(concat('ZOOStock.js'))
-    .pipe(gulp.dest('./dist'))
+  
 );
+
+gulp.task('minifyFooter', () =>
+  gulp.src('./scripts/footer/*.js')
+    .pipe(minify({
+      mangle: { keepClassName: true }
+    }))
+    .pipe(concat('ZOOStock.footer.js'))
+    .pipe(gulp.dest('./public'))
+)
