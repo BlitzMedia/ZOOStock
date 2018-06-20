175e6ec197b35991dcaebe50fff99414014767e5
gorkolas <gorka.molero@gmail.com>
2018-06-20 14:21:44 +0200

    [Feature] Added new target for filters: CABLES

--- scripts/footer/ZOOStock.04.CustomFilterSettings.js
+++ scripts/footer/ZOOStock.04.CustomFilterSettings.js
@@ -1,112 +1,240 @@
 window.customFilterSettings = {
-    'targets': [
-      {
-        container: '.collection-5b291a29575d1ff09437761d #block-yui_3_17_2_1_1529421639296_3852',
-        items: '.summary-item',
-        settings: {
-          	performanceMode:true,
-            showItemsCount: true, //hide items count
-            position: 'left',
-          	keepDropdownsOpen: false,
-            requestAttrWithAjax: false,
-          	mobilePanel: { enabled: true },
-          	isotope: {
-            	enabled: false,
-              columnWidth: '30%'
+  'targets': [
+    {
+      // Transceivers
+      container: '.collection-5b291a29575d1ff09437761d #block-yui_3_17_2_1_1529421639296_3852',
+      items: '.summary-item',
+      settings: {
+        performanceMode:true,
+        showItemsCount: true, //hide items count
+        position: 'left',
+        keepDropdownsOpen: false,
+        requestAttrWithAjax: false,
+        mobilePanel: { enabled: true },
+        isotope: {
+          enabled: false,
+          columnWidth: '30%'
+        },
+        simpleFilter: {
+          anim: true,
+          show: {
+            effect: 'slideLeft',
+            transitionDuration: 400,
+            stagger: 100
+          },
+          hide: {
+            effect: 'slideRight',
+            transitionDuration: 300,
+            stagger: 60
+          }
+        },
+        filter: {
+          category:false,
+          tag: false,
+          items: [
+            {
+              name: 'Vendor',
+              multiple: true,
+              getAttr: 'categories'
             },
-            simpleFilter: {
-              anim: true,
-              show: {
-                effect: 'slideLeft',
-                transitionDuration: 400,
-                stagger: 100
+            {
+              name: 'Form Factor',
+              multiple: true,
+              getAttr: (el, data) => {
+                //el is the real html node of item and data is json-data of this item (product)
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  //now loop through all product variants and build array of sizes
+                  arr = Y.Array.map(variants, function(variant) {
+                    var formFactor = variant['Form Factor'] || '';
+                    return formFactor;
+                  })
+                }
+                return arr;
               },
-              hide: {
-                effect: 'slideRight',
-                transitionDuration: 300,
-                stagger: 60
-              }
             },
-            filter: {
-            	category:false,
-            	tag: false,
-              items: [
-              {
-                name: 'Vendor',
-                multiple: true,
-                getAttr: 'categories'
+            {
+              name: 'Data Rate',
+              multiple: true,
+              getAttr: function(el, data) {
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  arr = Y.Array.map(variants, function(variant) {
+                    var dataRate = variant['Data Rate'] || '';
+                    return dataRate;
+                  })
+                }
+                return arr;
               },
-              {
-                name: 'Form Factor',
-                multiple: true,
-                getAttr: (el, data) => {
-                  //el is the real html node of item and data is json-data of this item (product)
-                    var arr = [];
-                    var variants = data.variants;
-                    if (variants && variants.length) {
-                      //now loop through all product variants and build array of sizes
-                      arr = Y.Array.map(variants, function(variant) {
-                        var formFactor = variant['Form Factor'] || '';
-                        return formFactor;
-                      })
-                    }
-                    return arr;
-                },
-              },
-              {
-                name: 'Data Rate',
-                multiple: true,
-                getAttr: function(el, data) {
-                    var arr = [];
-                    var variants = data.variants;
-                    if (variants && variants.length) {
-                        arr = Y.Array.map(variants, function(variant) {
-                            var dataRate = variant['Data Rate'] || '';
-                            return dataRate;
-                        })
-                    }
-                    return arr;
-                },
-              },
-              {
-                name: 'Media Type',
-                multiple: true,
-                getAttr: function(el, data) {
-                    var arr = [];
-                    var variants = data.variants;
-                    if (variants && variants.length) {
-                        arr = Y.Array.map(variants, function(variant) {
-                            var mediaType = variant['Media Type'] || '';
-                            return mediaType;
-                        })
-                    }
-                    return arr;
-                },
-              }]
             },
-            sort: {
-              enabled: false,
-              items: [{
-                name: 'Price',
-                order: 'asc|desc'
-              }]
-            },
-            search: {
-              positionOrder: 100,
-              text: 'Search',
-              searchFunc: function(el) {
-                var result = '',
-                textContent = '',
-                classes = '';
-                el = el._node ? el._node : el;
-                textContent = el.textContent.trim();
-                classes = el.className;
-                result = textContent + ' ' + classes;
-                return result;
+            {
+              name: 'Media Type',
+              multiple: true,
+              getAttr: function(el, data) {
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  arr = Y.Array.map(variants, function(variant) {
+                    var mediaType = variant['Media Type'] || '';
+                    return mediaType;
+                  })
+                }
+                return arr;
               },
-              enabled: true
-           }
+            }
+          ]
+        },
+        sort: {
+          enabled: false,
+          items: [{
+            name: 'Price',
+            order: 'asc|desc'
+          }]
+        },
+        search: {
+          positionOrder: 100,
+          text: 'Search',
+          searchFunc: function(el) {
+            var result = '',
+            textContent = '',
+            classes = '';
+            el = el._node ? el._node : el;
+            textContent = el.textContent.trim();
+            classes = el.className;
+            result = textContent + ' ' + classes;
+            return result;
+          },
+          enabled: true
         }
+      }
+    },{
+      // Cables
+      container: '.collection-5b293c2f758d46d3d609fd82 #block-yui_3_17_2_1_1529429054890_7723',
+      items: '.summary-item',
+      settings: {
+        performanceMode:true,
+        showItemsCount: true, //hide items count
+        position: 'left',
+        keepDropdownsOpen: false,
+        requestAttrWithAjax: false,
+        mobilePanel: { enabled: true },
+        isotope: {
+          enabled: false,
+          columnWidth: '30%'
+        },
+        simpleFilter: {
+          anim: true,
+          show: {
+            effect: 'slideLeft',
+            transitionDuration: 400,
+            stagger: 100
+          },
+          hide: {
+            effect: 'slideRight',
+            transitionDuration: 300,
+            stagger: 60
+          }
+        },
+        filter: {
+          category:false,
+          tag: false,
+          items: [
+            {
+              name: 'Vendor',
+              multiple: true,
+              getAttr: 'categories'
+            },
+            {
+              name: 'Media Type',
+              multiple: true,
+              getAttr: function(el, data) {
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  arr = Y.Array.map(variants, function(variant) {
+                    var mediaType = variant['Media Type'] || '';
+                    return mediaType;
+                  })
+                }
+                return arr;
+              },
+            },
+            {
+              name: 'Data Rate',
+              multiple: true,
+              getAttr: function(el, data) {
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  arr = Y.Array.map(variants, function(variant) {
+                    var dataRate = variant['Data Rate'] || '';
+                    return dataRate;
+                  })
+                }
+                return arr;
+              },
+            },
+            {
+              name: 'Reach',
+              multiple: true,
+              getAttr: (el, data) => {
+                //el is the real html node of item and data is json-data of this item (product)
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  //now loop through all product variants and build array of sizes
+                  arr = Y.Array.map(variants, function(variant) {
+                    var reach = variant['Reach'] || '';
+                    return Reach;
+                  })
+                }
+                return arr;
+              },
+            },
+            {
+              name: 'Connector Type',
+              multiple: true,
+              getAttr: (el, data) => {
+                //el is the real html node of item and data is json-data of this item (product)
+                var arr = [];
+                var variants = data.variants;
+                if (variants && variants.length) {
+                  //now loop through all product variants and build array of sizes
+                  arr = Y.Array.map(variants, function(variant) {
+                    var connectorType = variant['Connector Type'] || '';
+                    return connectorType;
+                  })
+                }
+                return arr;
+              }
+            }
+          ]
+        },
+        sort: {
+          enabled: false,
+          items: [{
+            name: 'Price',
+            order: 'asc|desc'
+          }]
+        },
+        search: {
+          positionOrder: 100,
+          text: 'Search',
+          searchFunc: function(el) {
+            var result = '',
+            textContent = '',
+            classes = '';
+            el = el._node ? el._node : el;
+            textContent = el.textContent.trim();
+            classes = el.className;
+            result = textContent + ' ' + classes;
+            return result;
+          },
+          enabled: true
+        }
+      }
     }
   ]
 };
